26f0c3f40aa3eea32ed52db7027b64374b09c6c5
Seung-Gi0301 <studiosnp0301@gmail.com>
2023-04-17 15:11:53 +0900

    [Test] - interfaces & ERC standard structure

--- main/src/contracts/ERC721Connector.sol
+++ main/src/contracts/ERC721Connector.sol
@@ -2,9 +2,10 @@
 pragma solidity ^0.8.0;
 
 import './ERC721Metadata.sol';
+import './ERC721Enumerable.sol';
 
-contract ERC721Connector is ERC721Metadata {
-    constructor(string memory name, string memory studentNumber) ERC721Metadata(name, studentNumber){
+contract ERC721Connector is ERC721Metadata, ERC721Enumberable {
+    constructor(string memory name, string memory symbol) ERC721Metadata(name, symbol){
 
     }
-} 
+}
